9a13b8ee983245a25695c0d80c50dae73080ae5e
Dalton <DY@test.com>
2022-10-23 23:18:15 -0500

    finalizing assignment

--- practiceClass.py
+++ practiceClass.py
@@ -12,3 +12,49 @@ change the status attribute to False.
 2) a Booking class that has attributes for customer name and
 seat number. Create accessor methods for both attributes.'''
         
+class Play:
+
+    def __init__(self,idnumber,name,num_seats,show_date,seat_status):
+
+        self.__idnumber = idnumber
+        self.__name = name
+        self.__num_seats = num_seats
+        self.__show_date = show_date
+        self.__seat_status = seat_status
+        self.__seat_status = True
+
+    def get_name(self):
+        return self.__name
+
+    def get_idnumber(self):
+        return self.__idnumber
+
+    def get_num_seats(self):
+        return self.__num_seats
+
+    def get_show_date(self):
+        return self.__show_date
+
+    def get_seat_status(self):
+        return self.__seat_status
+
+    def change_seat_status(self):
+        self.__seat_status = False
+
+    def seats_left(self):
+        self.__num_seats -= 1
+
+
+class Booking:
+
+    def __init__(self,client_name,seat_num):
+        self.__client_name = client_name
+        self.__seat_num = seat_num
+
+    def get_client_name(self):
+        return self.__client_name
+    
+    def get_seat_num(self):
+        return self.__seat_num
+
+
--- practiceProgram.py
+++ practiceProgram.py
@@ -1,4 +1,4 @@
-import practiceClassKEY as p
+import practiceClass as pc
 import csv
 
 
@@ -35,7 +35,11 @@ play class that has id 9587
 NOTE: Do not hard code the values to create the instance but use
 keys and values from the dictionary '''
 
+for plays in shows:
 
+    if shows[plays]["id"] == 9587:
+        
+        hamilton = pc.Play(shows[plays]["id"], shows[plays]["name"],shows[plays]["capacity"],shows[plays]["event_date"], True)
 
 '''using the bookings.csv file process only those 
 reservations for the same play (9587). Create an 
@@ -47,10 +51,32 @@ error message as shown in output.JPG'''
 
 #open the csv file in read mode
 
+bookings = open("bookings.csv", "r")
 
 #create a csv object from the file object from the step above
 
-
+csvfile = csv.reader(bookings,delimiter=",")
+next(csvfile)
 
 # use a for loop to iterate through each record in the bookings file
 
+for line in csvfile:
+
+    if int(line[0]) == 9587:
+
+        hamilton.seats_left()
+        pc.Booking(line[1],line[2])
+
+        if hamilton.get_num_seats() < 0:
+
+            hamilton.change_seat_status()
+            print()
+            print()
+            print()
+            print("***** ERROR *****")
+            print("Guest Name: ", line[1])
+            print("Sorry, the show:", hamilton.get_name(), "is sold out")
+            print("**********")
+            print()
+            print()
+            print()
